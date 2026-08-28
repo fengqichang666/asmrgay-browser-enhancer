2cb66778bafe7a6a7f110f7df92e3183ba9e284e
fengqichang <2309997549@qq.com>
2026-08-28 15:57:30 +0800

    fix: invalidate cached player build

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "asmrgay-index-viewer-v2";
+const CACHE = "asmrgay-index-viewer-v3";
 const ASSETS = ["./", "./index.html", "./app.js", "./styles.css", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())); });
